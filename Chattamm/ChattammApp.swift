27034770af41f433d1face77045c54e74f351140
Francesco Paciello <kekkopacio@gmail.com>
2021-02-22 16:39:01 +0100

    Initial Commit

---
+++ Chattamm/ChattammApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattammApp.swift
+//  Chattamm
+//
+//  Created by Kekko Paciello on 22/02/21.
+//
+
+import SwiftUI
+
+@main
+struct ChattammApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
